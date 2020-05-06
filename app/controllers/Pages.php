f32319b931d5b42ae5ff87d4c3a7abb18740d9bf
Raymond Chen <raymondchen86@hotmail.com>
2020-05-06 02:56:16 -0700

    mapping methods and parameters

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,6 +1,15 @@
 <?php
     class Pages {
         public function __construct() {
-            echo 'Pages loaded';
+            // echo 'Pages loaded';
+        }
+
+        public function index(){
+            
+        }
+
+        public function about($id){
+            // echo 'This is about';
+            echo $id;
         }
     }
