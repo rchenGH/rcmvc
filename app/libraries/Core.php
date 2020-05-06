1026059d1a3a5338199c6c636ed5cf49abe7f78f
Raymond Chen <raymondchen86@hotmail.com>
2020-05-06 01:31:03 -0700

    loading the controller from the URL

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -11,10 +11,34 @@
         protected $params = [];
 
         public function __construct(){
-            $this->getURL();
+            // print_r($this->getURL());
+
+            $url = $this->getURL();
+
+            // Look in controllers for first value
+            // currently in index.php
+            if(isset($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+                // if exists, set as current controller
+                // will overwrite Pages, which is the default
+                $this->currentController = ucwords($url[0]);
+
+                // Unset 0 index
+                unset($url[0]);
+            }
+
+            // Require the controller
+            require_once '../app/controllers/' . $this->currentController . '.php';
+
+            // Instantiate controller class
+            $this->currentController = new $this->currentController;
         }
 
         public function getURL(){
-            echo $_GET['url'];
+            if(isset($_GET['url'])){
+                $url = rtrim($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
+                return $url;
+            }
         }
     }
