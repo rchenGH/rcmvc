8054248ae47d204ba7d96789a0d66d308de1c2c9
Raymond Chen <raymondchen86@hotmail.com>
2020-05-08 03:25:44 -0700

    add file config and autoloader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
-    // Load libraries
-    require_once 'libraries/Core.php';
-    require_once 'libraries/Controller.php';
-    require_once 'libraries/Database.php';
+    // Load Config
+    require_once 'config/config.php';
+
+    // Autoload Core Libraries
+    // Class name must match the file name for auto loader to work
+    spl_autoload_register(function($className){
+        require_once 'libraries/' . $className . '.php';
+    });
---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+    // App Root
+    // Can be called anywhere in the app
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL Root
+    define('URLROOT', 'http://localhost:8080/rcmvc/pages');
+
+    // Site Name
+    define('SITENAME', 'RCMVC');
