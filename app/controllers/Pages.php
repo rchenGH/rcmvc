7166500272ee912b8ae28932fc6ca15d7dee86ba
Raymond Chen <raymondchen86@hotmail.com>
2020-05-08 02:55:45 -0700

    add base controller class

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,11 +1,11 @@
 <?php
-    class Pages {
+    class Pages extends Controller{
         public function __construct() {
             // echo 'Pages loaded';
         }
 
         public function index(){
-            
+            $this->view('hello');
         }
 
         public function about($id){
